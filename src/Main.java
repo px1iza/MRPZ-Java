8379987e79265cb5c3caf8f6e19f9d16585ccae1
Liza <rabirokhliza@gmail.com>
2026-02-26 21:46:04 +0200

    done lab1

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Rabirokh Elizaveta");
+        System.out.println("121 Group");
+        System.out.println();
+
+        int matrix[][] = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int temp = matrix[i][0];
+            matrix[i][0] = matrix[i][i];
+            matrix[i][i] = temp;
+        }
+        System.out.println();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            sum = matrix[i][0] + sum;
+        }
+        System.out.println(sum);
+
+    }
+}
